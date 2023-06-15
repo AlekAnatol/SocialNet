caa3898ced67b08960832edc47b3be31018f1fe9
Globa EA <89096346135a@gmail.com>
2023-06-15 18:14:15 +0300

    Создала кастомный контрол LikesCounterControlView
    Создала кастомный контрол LikesCounterControlView, который состоит из кнопки heartButton и лейбла countLabel с указанием количества лайков.
    При нажатии на heartButton картинка на кнопке либо закрашивается, либо становится не закрашенной, информация на лейбле изменяется.
    Изменения картинки на кнопке и данных на лейбле анимированы.
    Первичное количество лайков - номер айтема в коллекции.

---
+++ SocialNet/SocialNet/CustomElements/LikesControlView/LikesCounterControlView.swift
@@ -0,0 +1,20 @@
+//
+//  LikesCounterControlView.swift
+//  SocialNet
+//
+//  Created by Екатерина Алексеева on 14.06.2023.
+//
+
+import UIKit
+
+class LikesCounterControlView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
